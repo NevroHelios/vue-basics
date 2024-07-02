71cab68a93ebba7c28430e8750e113e66354657b
Arka Dash <dasharka05@gmail.com>
2024-07-02 23:25:05 +0530

    initiaal commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar
+        :pages="pages"
+        :activePage="activePage"
+        :nav-link-click="(index) => activePage = index"
+    />
+    <PageViewer
+        v-if="pages.length > 0"
+        :page="pages[activePage]"
+    />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import PageViewer from './components/PageViewer.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components : {
+        NavBar,
+        PageViewer
+    },
+    created() {
+        this.getPages()
+    },
+    data() {
+        return {
+            activePage : 0,
+            pages : []
+        }
+    },
+    methods : {
+        async getPages() {
+            let res = await fetch('pages.json');
+            let data = await res.json()
+
+            this.pages = data;
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,64 @@
+<template>
+    <nav
+        :class="['navbar', 'navbar-expand-lg', `navbar-${theme}`, `bg-${theme}`]"
+    >
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My vue</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                    <NavBarLink
+                        :page="page"
+                        :isActive="activePage === index"
+                        @click.prevent="navLinkClick(index)"
+                    />
+                </li>
+            </ul>
+            <form class="d-flex">
+                <button
+                    class="btn btn-outline-success"
+                    @click.prevent="changeTheme()"
+                    >Toggle</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+import NavBarLink from './NavBarLink.vue';
+
+export default {
+    components  : {
+        NavBarLink
+    },
+    props : ['pages', 'activePage', 'navLinkClick'],
+    created () {
+        this.getThemeSetting()
+    },
+    data() {
+        return {
+            theme : 'light'
+        }
+    },
+    methods : {
+        changeTheme() {
+            let theme = 'light';
+                if (this.theme == 'light') {
+                    theme = 'dark';
+                }
+
+            this.theme = theme;
+            this.storeThemeSetting()
+            
+        },
+        storeThemeSetting() {
+            localStorage.setItem("theme", this.theme)
+        },
+        getThemeSetting() {
+            let theme = localStorage.getItem("theme")
+                if (theme) {
+                    this.theme = theme;
+                }
+        }
+    },
+}
+</script>
